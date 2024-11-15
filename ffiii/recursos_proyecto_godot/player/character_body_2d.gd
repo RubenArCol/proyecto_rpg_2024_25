ee7c1f04677d561006343604f7f462055456a961
RubenArCol <124272803+RubenArCol@users.noreply.github.com>
2024-11-15 19:44:26 +0100

    ajusto cámara y movimiento

--- ffiii/recursos_proyecto_godot/player/character_body_2d.gd
+++ ffiii/recursos_proyecto_godot/player/character_body_2d.gd
@@ -4,7 +4,7 @@ var direccion: Vector2 = Vector2.ZERO
 var velocidad:int = 250
 
 func _process(_delta):
-	direccion = Input.get_vector("left", "up", "right", "down")
+	direccion = Input.get_vector("left", "right", "up", "down")
 	
 	
 func _physics_process(_delta):
