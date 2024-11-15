1779b032a3452ddceceacbdd9a8c3523a313a949
RubenArCol <124272803+RubenArCol@users.noreply.github.com>
2024-11-15 19:18:44 +0100

    movimiento del personaje

---
+++ ffiii/recursos_proyecto_godot/player/character_body_2d.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+var direccion: Vector2 = Vector2.ZERO
+var velocidad:int = 250
+
+func _process(_delta):
+	direccion = Input.get_vector("left", "up", "right", "down")
+	
+	
+func _physics_process(_delta):
+	velocity = direccion * velocidad
+	move_and_slide()
+	
