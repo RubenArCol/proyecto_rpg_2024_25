d2ff47f11006cb20621f77106fcf57272ba3a806
RubenArCol <124272803+RubenArCol@users.noreply.github.com>
2024-11-17 19:53:12 +0100

    feat/position
    adding player initial spawn and speed,via script and setting camera zoom

--- ffiii/recursos_proyecto_godot/player/character_body_2d.gd
+++ ffiii/recursos_proyecto_godot/player/character_body_2d.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 var direccion: Vector2 = Vector2.ZERO
-var velocidad:int = 250
+var velocidad:int = 100
 
 func _process(_delta):
 	direccion = Input.get_vector("left", "right", "up", "down")
