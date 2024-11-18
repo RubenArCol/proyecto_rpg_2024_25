39676487f6c41ccad4b9a77cc23ccf03e05d1c59
RubenArCol <124272803+RubenArCol@users.noreply.github.com>
2024-11-18 16:41:23 +0100

    feat/reorder

--- ffiii/recursos_proyecto_godot/Scripts/teleport.gd
+++ ffiii/recursos_proyecto_godot/Scripts/teleport.gd
@@ -3,3 +3,4 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		body.set_position($Destination.global_position)
+		print($Destination.global_position)
