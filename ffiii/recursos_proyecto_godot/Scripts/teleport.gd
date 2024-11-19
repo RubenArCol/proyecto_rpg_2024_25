b5ab0caedfd8f3cd1a3371afc6a8edf9fad45a89
RubenArCol <124272803+RubenArCol@users.noreply.github.com>
2024-11-19 15:19:27 +0100

    feat/implements signaling and comments

--- ffiii/recursos_proyecto_godot/Scripts/teleport.gd
+++ ffiii/recursos_proyecto_godot/Scripts/teleport.gd
@@ -4,7 +4,6 @@ signal tp()
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		emit_signal("tp") # needs implementation
-		body.toggle_movement()
-		await  Load.transition()
-		body.global_position = $Destination.global_position
+		emit_signal("tp") # emit signal
+		await  Load.transition() # aprox 1.2 secs
+		body.global_position = $Destination.global_position # change position
--- ffiii/recursos_proyecto_godot/player/character_body_2d.gd
+++ ffiii/recursos_proyecto_godot/player/character_body_2d.gd
@@ -4,30 +4,35 @@ var direccion:Vector2 = Vector2.ZERO
 var velocidad:int = 100
 var movement:bool = true
 
+func _ready() -> void:
+	var tp_nodes = get_tree().get_nodes_in_group("Teleports") # gets every tp in the group
+	for tp_node in tp_nodes:
+		tp_node.connect("tp", Callable(self, "toggle_movement")) # connects the function to the player
+
 func _process(_delta):
 	if movement: 
-		direccion = Input.get_vector("left", "right", "up", "down")
+		direccion = Input.get_vector("left", "right", "up", "down") # direction
 	else:
-		direccion = Vector2.ZERO
+		direccion = Vector2.ZERO # doesn't allow changing direction
 	
 
 func _physics_process(_delta):
 	if movement:
-		velocity = direccion * velocidad
+		velocity = direccion * velocidad 
 	else:
-		velocity = Vector2.ZERO
+		velocity = Vector2.ZERO # doesn't allow movement
 	move_and_slide()
 
 func toggle_movement():
-	movement = not movement
+	movement = not movement # movement to false
 	await wait(1.2)
-	movement = not movement
+	movement = not movement # movement to true
 
-func wait(tiempo: float) -> void: # gracias stack overflow ;3
-	var timer = Timer.new()  # Crear un nuevo Timer
-	timer.one_shot = true    # Para que solo se dispare una vez
-	timer.wait_time = tiempo # Configurar el tiempo de espera
-	add_child(timer)         # Añadir el Timer a la escena
-	timer.start()            # Iniciar el Timer
-	await timer.timeout      # Esperar a que termine
-	timer.queue_free()       # Eliminar el Timer después de usarlo
+func wait(tiempo: float) -> void: # stack overflow ;3
+	var timer = Timer.new()  # new timer
+	timer.one_shot = true    # one use only
+	timer.wait_time = tiempo # set timer to "tiempo"
+	add_child(timer)         # add timer
+	timer.start()
+	await timer.timeout      # wait for ending
+	timer.queue_free()       # deletes the timer after ending
