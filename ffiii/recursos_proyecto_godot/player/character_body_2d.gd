4b1c40be9bcbb8cb82960eb024c7df337c81c456
RubenArCol <124272803+RubenArCol@users.noreply.github.com>
2024-12-10 19:25:46 +0100

    feat/add sprite

--- ffiii/recursos_proyecto_godot/player/character_body_2d.gd
+++ ffiii/recursos_proyecto_godot/player/character_body_2d.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
 var direccion:Vector2 = Vector2.ZERO
-var velocidad:int = 100
+var velocidad:int = 30
 var movement:bool = true
 
-#add player's inventory 
+# player's inventory
 @export var inv: Inv 
+@onready var sprite_2d: AnimatedSprite2D = $Sprite2D
 
 func _ready() -> void:
 	var tp_nodes = get_tree().get_nodes_in_group("Teleports") # gets every tp in the group
@@ -14,17 +15,26 @@ func _ready() -> void:
 
 func _physics_process(_delta):
 	if movement:
-		velocity = direccion * velocidad 
+		velocity = direccion * velocidad
 		direccion = Input.get_vector("left", "right", "up", "down") # direction
+
+		# animations
+		if direccion != Vector2.ZERO:
+			sprite_2d.play("run")
+			# player's looking direction
+			sprite_2d.flip_h = direccion.x < 0
+		else: # player idle
+			sprite_2d.play("idle")
 	else:
 		direccion = Vector2.ZERO # doesn't allow changing direction
 		velocity = Vector2.ZERO # doesn't allow movement
+		sprite_2d.play("idle") # Cuando no se puede mover, poner idle
 	move_and_slide()
 
 func toggle_movement():
-	movement = not movement # movement to false
+	movement = not movement # Toggle movement state
 	await wait(1.2)
-	movement = not movement # movement to true
+	movement = not movement # Toggle movement state back to true
 
 func wait(tiempo: float) -> void: # stack overflow ;3
 	var timer = Timer.new()  # new timer
